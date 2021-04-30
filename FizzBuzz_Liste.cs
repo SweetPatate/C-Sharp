6382877ac781953ce18284feeeb27d0ec7d92f5b
Stéphane Meresse <45168141+SweetPatate@users.noreply.github.com>
2021-04-30 22:01:50 +0200

    Update and rename FizzBuzz.cs to FizzBuzz_Liste.cs

---
+++ FizzBuzz_Liste.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+
+class FizzBuzz
+{
+        /*
+    FizzBuzz :
+    if the number is divisible by 3: write Fizz
+    if the number is divisible by 5: we write Buzz
+    if the number is divisible by 3 and by 5: write Fizzbuzz
+    otherwise: write the number
+    */
+    
+    static void Main(string[] args)
+    {
+        List<int> collection = new List<int>() { 1, 2, 5, 7, 8, 10, 15 }; ;
+
+        foreach (int x in collection)
+        {
+            if (x % 3 == 0)
+            {
+                Console.Write("Fizz");
+            }
+            if (x % 5 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            else
+            {
+                Console.Write(x);
+            }
+            Console.WriteLine();
+        }
+    }
+}
