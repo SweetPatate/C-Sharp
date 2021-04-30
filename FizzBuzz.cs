a4c31051a0cf1216b04fdfddd2f9774f12e083a8
Stéphane Meresse <45168141+SweetPatate@users.noreply.github.com>
2021-04-30 22:07:26 +0200

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FizzBuzz
+{
+    /*
+    FizzBuzz :
+    if the number is divisible by 3: write Fizz
+    if the number is divisible by 5: we write Buzz
+    if the number is divisible by 3 and by 5: write Fizzbuzz
+    otherwise: write the number
+    */
+    static void Main(string[] args)
+    {
+        for (int x=1; x<=100; x++)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (x % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
