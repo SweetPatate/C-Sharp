55e8b107f25e534564b90cfccea24f4f7536634a
Stéphane Meresse <45168141+SweetPatate@users.noreply.github.com>
2021-05-24 02:02:30 +0200

    Add files via upload

---
+++ liste_objet(FILM)/labs/Movies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs
+{
+    class Movies
+    {
+        private DateTime realeaseDate;
+        private string name;
+        private string genre;
+
+        public string Name { get => name; set => name = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public DateTime RealeaseDate { get => realeaseDate; set => realeaseDate = value; }
+
+        public override String ToString()
+        {
+            return "The Movie is " + Name + " genre : " + Genre + " realease Date : " + RealeaseDate.ToString("dd/M/yyyy");
+        }
+    }
+}
